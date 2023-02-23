b7b1b7de91c6d8dfa4e61fd3ebb15e8eed209b69
badfishtechnologies <bkane@badfishtechnologies.com>
2023-02-22 22:00:25 -0500

    Set up React Native TravelApp

---
+++ src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+import Title from '../../components/Title';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Title />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default React.memo(Home);
