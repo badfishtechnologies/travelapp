7fb7c45e7ac9b064292b71955ad641f33c17602d
badfishtechnologies <bkane@badfishtechnologies.com>
2023-02-23 06:10:28 -0500

    added comment

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import Title from '../../components/Title';
-
+//added comment
 const Home = () => {
   return (
     <SafeAreaView>
